1d84b1ae018e60b5d5f4acb6facdac823f21fa15
Richard Ludwig <Richard.Ludwig+gh@gmail.com>
2023-09-17 20:09:36 +0200

    Add AssemblyInfo to project

---
+++ VPinballX.starter/Properties/AssemblyInfo.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("VPinballX.starter")]
+[assembly: AssemblyDescription("VPinballX starter")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Visual Pinball")]
+[assembly: AssemblyProduct("VPinballX.starter")]
+[assembly: AssemblyCopyright("Copyright © 2023")]
+[assembly: AssemblyTrademark("Richard Ludwig")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.9.1")]
+[assembly: AssemblyFileVersion("0.9.1")]
+[assembly: AssemblyInformationalVersion("0.9.1")]
